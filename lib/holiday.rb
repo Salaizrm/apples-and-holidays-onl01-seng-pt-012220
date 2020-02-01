0973ace8dd8be4adf4d1de53ed1ad823922feb96
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-01 01:50:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,9 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do | season, holiday|
      puts "#{season.capitalize}:"
-     holiday.each do |holiday, item|
-       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
-end
+    end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
